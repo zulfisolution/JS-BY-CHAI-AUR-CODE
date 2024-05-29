878edb037e50f93d275efc4a353c23241056ef88
Zulfiqar Ali Alvi <93054700+zulfisolution@users.noreply.github.com>
2024-05-29 19:08:19 +0000

    Datatypes

---
+++ basicjs/datatype.js
@@ -0,0 +1,31 @@
+"use strict"; //Treat all js code as a newer version
+//alert(3+3)    we dont use like that in  node js but in  browser console
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Zulfi")
+
+
+let name = "Zulfi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.log(typeof state)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object LATER WE SEE
+
+//console.log(typeof undefined); // undefined
+//console.log(typeof null); // object
+
