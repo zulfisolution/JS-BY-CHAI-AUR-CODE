c3eeefd1ff8b244c65b9a9e9da9b82785468c986
Zulfiqar Ali Alvi <93054700+zulfisolution@users.noreply.github.com>
2024-05-29 16:27:28 +0000

    student info

---
+++ basicjs/variables.js
@@ -0,0 +1,8 @@
+let Name = "Name : NASU"
+const RollNumber  ="RollNumber : 111"
+let Class = "Class : 9"
+let Sec = "Sec : B"
+const Gender = "Gender : male"
+let PhoneNumber = "PhoneNumber : +923151025258" 
+console.table([Name,RollNumber,Class,Sec,Gender,PhoneNumber])
+
