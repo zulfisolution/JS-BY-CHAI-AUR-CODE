ec3c00038de64d1ef06b559dc7cac5567f4b5ef7
Zulfiqar Ali Alvi <93054700+zulfisolution@users.noreply.github.com>
2024-05-30 10:33:24 +0000

    DataType Conversion

---
+++ basicjs/Datatypeconversion.js
@@ -0,0 +1,69 @@
+
+let score = "33abc"    
+console.log(score)               //print score value 
+console.log(typeof (score))      //print data type  
+
+let CovertValueInNumber = Number(score)   //converting into Number by using "Number()"  to convert into  string "String()"
+console.log(typeof CovertValueInNumber)     // printing type  of that converted  number 
+console.log(CovertValueInNumber)            // printing datatype  of that converted  number
+
+
+
+
+//     NULL is Object
+
+// let Empty= null 
+// console.log(Empty)
+// console.log(typeof (Empty))
+
+// let CovertIntoNum = Number(Empty)
+// console.log(CovertIntoNum)
+// console.log(typeof (CovertIntoNum))
+
+
+
+//ConvertIntoString
+// let NotDefine = undefined
+// console.log(NotDefine)
+// console.log(typeof (NotDefine))
+
+// let ConvertIntoString =  String(NotDefine)
+// console.log(ConvertIntoString)
+// console.log(typeof  ConvertIntoString)
+
+
+//Bolean true/false
+
+// let Boleans = true
+// console.log(Boleans)
+// console.log(typeof Boleans)
+
+// let ConvertIntoNums = Number(Boleans)
+// console.log(ConvertIntoNums)
+// console.log(typeof ConvertIntoNums)
+
+
+// let IsLogedIn = "hatesh"
+// let BoleanIslogedIn = Boolean(IsLogedIn)
+// console.log(BoleanIslogedIn)
+// console.log(IsLogedIn)
+// console.log(typeof IsLogedIn)
+// console.log(typeof (BoleanIslogedIn))
+
+
+// let IsLogedIn = true
+// console.log(IsLogedIn)
+// console.log(typeof IsLogedIn)
+// let ConvertInBolean = Boolean(IsLogedIn)
+// console.log(ConvertInBolean)
+// console.log(typeof ConvertInBolean)
+
+
+
+
+
+
+
+
+
+
